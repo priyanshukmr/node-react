b2855b627f94d6fbba4896979a2dafc1dd3579dd
Priyanshu Kumar <priyanshu.kanpur@gmail.com>
2021-10-24 03:13:04 +0530

    init express+react

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app  = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = 3000;
+
+console.log(__dirname);
+
+app.get('/', (req,res) => {
+    res.sendFile('index.html', {root: __dirname});
+})
+
+app.listen(port, () => {
+    console.log('Node server listening on ' + port);
+});
