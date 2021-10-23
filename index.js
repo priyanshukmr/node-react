2374ba5087dd122953d7050c096c65587429b272
Priyanshu Kumar <priyanshu.kanpur@gmail.com>
2021-10-24 03:52:52 +0530

    change port for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app  = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 console.log(__dirname);
 
